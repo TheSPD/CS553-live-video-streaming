c002f6d2004bff84d56922c6419a9448717d9fe5
Shreepad Patil <shreepad007@gmail.com>
2017-11-23 23:28:22 -0500

    Added socket.io, express, and working apis to stream

--- index.js
+++ index.js
@@ -1,47 +1,39 @@
-var express = require('express'),
-	ffmpeg = require('fluent-ffmpeg'),
-	fs = require('fs'),
-	io = require('socket.io')(http)
-
+var express = require('express')
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.use(express.static(__dirname + '/app'))
+app.use(express.static('app'))
 
-// app.get('/', function(req, res) {
-// 	res.sendFile('index.html')
-// });
+var streams = [];
 
-// app.head('/video/:filename', function (req, res) {
-// 	console.log('Head')
-// 	res.set('Transfer-Encoding', 'chunked').send();
-// })
+app.post('/sendStream/:streamName', (request, response) => {
+	let streamName = request.params.streamName
 
-// app.get('/video/:filename', function(req, res) {
-// 	// res.contentType('mpegts')
-// 	var pathToMovie = __dirname + '/samples/' + req.params.filename
-// 	console.log(pathToMovie)
-// 	var proc = ffmpeg(pathToMovie)
-// 		.format('mpegts')
-// 		// .size('320x?')
-// 		.videoBitrate('512k')
-// 		.videoCodec('mpeg1video')
-// 		.fps(24)
-// 		// .audioBitrate('96k')
-// 		// .audioCodec('aac')
-// 		// .audioFrequency(22050)
-// 		// .audioChannels(2);
-// 		.native()
-// 		// .format('mpegts')
-// 		.size('640x480')
-// 		// setup event handlers
-// 		.on('end', function() {
-// 		  console.log('file has been converted succesfully');
-// 		})
-// 		.on('error', function(err) {
-// 		  console.log('an error happened: ' + err.message);
-// 		})
-// 		// save to stream
-// 		.pipe(res, {end:true});
-// });
+	if(streams.indexOf(streamName) < 0){
+		streams.push(streamName)
 
-app.listen(4000);
+		console.log('Starting streaming ' + streamName)
+		console.log(streams)
+		io.of('/' + streamName, (streamChannel) => {
+			
+			request.on('data', (data) => {
+				streamChannel.send(data)
+			});
+
+			request.on('end', () => {
+				streams.splice(streams.indexOf(streamName), 1)
+				console.log('Stopped streaming ' + streamName)
+			})
+		})		
+	}
+	else{
+		console.log('Stream with ' + streamName + ' exists!!!')
+		response.status(501)
+			.send("Stream already exists!!!")
+	}
+})
+
+http.listen(3000, function(){
+	console.log('listening on *:3000')
+});
