8c41fe2bd2f39ec1695bfe2de48573754e4c23c9
Shreepad Patil <shreepad007@gmail.com>
2017-11-24 00:02:35 -0500

    Added api for getting all streams

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require('express')
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
 app.use(express.static('app'))
 
-var streams = [];
+var streams = []
+
+app.get('/getStreams', (request, response) => {
+	response.send(streams)
+})
 
 app.post('/sendStream/:streamName', (request, response) => {
 	let streamName = request.params.streamName
@@ -14,12 +18,12 @@ app.post('/sendStream/:streamName', (request, response) => {
 		streams.push(streamName)
 
 		console.log('Starting streaming ' + streamName)
-		console.log(streams)
+		
 		io.of('/' + streamName, (streamChannel) => {
 			
 			request.on('data', (data) => {
 				streamChannel.send(data)
-			});
+			})
 
 			request.on('end', () => {
 				streams.splice(streams.indexOf(streamName), 1)
@@ -30,10 +34,10 @@ app.post('/sendStream/:streamName', (request, response) => {
 	else{
 		console.log('Stream with ' + streamName + ' exists!!!')
 		response.status(501)
-			.send("Stream already exists!!!")
+			.send('Stream with ' + streamName + ' exists!!!')
 	}
 })
 
 http.listen(3000, function(){
 	console.log('listening on *:3000')
-});
+})
